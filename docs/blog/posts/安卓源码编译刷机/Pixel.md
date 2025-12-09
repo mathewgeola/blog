246ebd2b91bc0152c56083fd7875e48a9f1525df
mathewgeola <mathewgeola@gmail.com>
2025-12-09 22:07:14 +0800

    2025/12/09 22:07

--- docs/blog/posts/安卓源码编译刷机/Pixel.md
+++ docs/blog/posts/安卓源码编译刷机/Pixel.md
@@ -1,6 +1,8 @@
 ---
 date:
   created: 2025-12-09
+categories:
+  - 安卓源码编译刷机
 ---
 
 # Pixel
--- hooks/socialmedia.py
+++ hooks/socialmedia.py
@@ -1,10 +1,11 @@
-from textwrap import dedent
-import urllib.parse
 import re
+import urllib.parse
+from textwrap import dedent
 
 x_intent = "https://twitter.com/intent/tweet"
 fb_sharer = "https://www.facebook.com/sharer/sharer.php"
 include = re.compile(r"blog/[1-9].*")
+
 
 def on_page_markdown(markdown, **kwargs):
     page = kwargs['page']
@@ -12,10 +13,10 @@ def on_page_markdown(markdown, **kwargs):
     if not include.match(page.url):
         return markdown
 
-    page_url = config.site_url+page.url
-    page_title = urllib.parse.quote(page.title+'\n')
+    page_url = config.site_url + page.url
+    page_title = urllib.parse.quote(page.title + '\n')
 
     return markdown + dedent(f"""
-    [Share on :simple-x:]({x_intent}?text={page_title}&url={page_url}){{ .md-button }}
-    [Share on :simple-facebook:]({fb_sharer}?u={page_url}){{ .md-button }}
+    [分享到 X:]({x_intent}?text={page_title}&url={page_url}){{ .md-button }}
+    [分享到 Facebook:]({fb_sharer}?u={page_url}){{ .md-button }}
     """)
